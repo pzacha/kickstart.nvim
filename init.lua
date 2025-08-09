6bee9e2a57733fdfab45c1263d3ee3193e372c8c
Piotr Zacha <pzacha11@gmail.com>
2025-08-09 12:40:03 +0200

    Add ruff config

--- init.lua
+++ init.lua
@@ -714,6 +714,12 @@ require('lazy').setup({
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
       },
+      formatters = {
+        ruff = {
+          command = 'uv',
+          args = { 'run', 'ruff', 'format', '--stdin-filename', '$FILENAME', '-' },
+        },
+      },
     },
   },
 
